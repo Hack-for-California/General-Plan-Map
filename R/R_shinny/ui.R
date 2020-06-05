f154c2f708295a787ae6eeedb5d18c825d573cb5
Lindsay Poirier <lindsay.poirier13@gmail.com>
2020-06-04 21:32:34 -0700

    Updates to About Text

--- R/R_shinny/ui.R
+++ R/R_shinny/ui.R
@@ -1,9 +1,11 @@
 ui <- fluidPage(
   titlePanel("California General Plan Mapping"),
-  sidebarPanel("Welcome to California General Plan Mapping project!
-               The purpose of this website is to visualize California general plans. 
-               Enter a search term into the textbox and press search to search the city 
-               of california general plans for a certain key words",  
+  sidebarPanel("Welcome to the California General Plan Mapping project!
+               The purpose of this website is to enable users to access, query, compare, 
+               and spatially visualize general plans in cities across California.
+               Enter a search term into the textbox, and click Search to query across the plans.
+               The map will highlight cities with general plans that include the search term.
+               Click a city on the map for links to the text of the city's plan.",  
                textInput("search", "", placeholder = "Enter Search Term"),
                actionButton("do", "Search"),
                actionButton("clear_map", "Clear Search"),
