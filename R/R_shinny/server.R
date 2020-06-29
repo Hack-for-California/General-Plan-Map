3260e7ff776500867091788ef30cce036420f73a
Lindsay Poirier <lindsay.poirier13@gmail.com>
2020-06-29 01:21:40 -0700

    Checks if user entered non-alpha character

--- R/R_shinny/server.R
+++ R/R_shinny/server.R
@@ -43,76 +43,79 @@ server <- function(input, output, session) {
                           search_string <- tolower(input$search)
                           search_results = search_wrapper(search_string)
                           
-                          if(length(search_results) == 0) {showModal(modalDialog(title = "Heads up!",
-                                                                                 "Search term not found."))}
-                          
-                          else {output$search_result <- renderUI({HTML("<div id='search_result_header'><h2>Search Result</h2></div")})
-                                search_city_bool = city_sf_final$search_matching %in% search_results
-                                city_sf_final$search = search_city_bool
+                          if(str_detect(search_string, '^[A-Za-z]+$', negate = TRUE)){showModal(modalDialog(title = "Heads up!",
+                                                                                                           "Please remove any numbers or special characters from the search string (e.g. community-oriented would be communityoriented)."))}
+                          else{
+                                if(length(search_results) == 0) {showModal(modalDialog(title = "Heads up!",
+                                                                                       "Search term not found."))}
                                 
-                                city_row <-subset(city_sf_final, 
-                                                  city_sf_final$search == 1)
-                                
-                                copy_pos_search = data.frame(city_row)
-                                table_display = select(copy_pos_search,
-                                                       c('NAMELSAD',
-                                                         'filepath_2'))
-                                
-                                table_display$filepath_2 = paste0("<a href='",
-                                                                  table_display$filepath_2,
-                                                                  "' target='_blank'>",
-                                                                  table_display$filepath_2,
-                                                                  "</a>")
-                                
-                                names(table_display) = c('Name', 
-                                                         'Link')
-                                
-                                #copy_city_sf_final = sapply(copy_city_sf_final,unlist)
-                                
-                                output$search_result_table <- DT::renderDataTable(table_display, 
-                                                                                  escape = FALSE)
-                                
-                                toClear = subset(city_sf_final, 
-                                                 city_sf_final$search == 0)
-                                
-                                #things for highlighting selected cities 
-                                leafletProxy("mymap", session) %>% 
-                                  addPolygons(data = city_row, 
-                                              layerId = ~NAME,
-                                              label = ~NAME,
-                                              color = "#444444",
-                                              opacity = 1.0,
-                                              weight = 0.5,
-                                              smoothFactor = 1.5,
-                                              fillColor = "#00bfc4",
-                                              fillOpacity = 0.9,
-                                              highlightOptions = highlightOptions(color = "white",
-                                                                                  weight = 1,
-                                                                                  bringToFront = TRUE), 
-                                              labelOptions = labelOptions(style = list("font-weight" = "normal",
-                                                                                       padding = "3px 8px"),
-                                                                          textsize = "10px",
-                                                                          direction = "auto"))
-                                if(nrow(toClear != 0)){leafletProxy("mymap", session) %>%
-                                                         addPolygons(data = toClear,
-                                                                     layerId = ~NAME,
-                                                                     label = ~NAME,
-                                                                     color = "#444444",
-                                                                     opacity = 1.0,
-                                                                     weight = 0.5,
-                                                                     smoothFactor = 1.5,
-                                                                     fillColor = "#f8766d",
-                                                                     fillOpacity = 0.9,
-                                                                     highlightOptions = highlightOptions(color = "white",
-                                                                                                         weight = 1,
-                                                                                                         bringToFront = TRUE),
-                                                                     labelOptions = labelOptions(style = list("font-weight" = "normal",
-                                                                                                              padding = "3px 8px"),
-                                                                                                 textsize = "10px",
-                                                                                                 direction = "auto"))}
-                                else{print("Term appears in all plans.")}
-                                }
-                                print("search for things complete")})
+                                else {output$search_result <- renderUI({HTML("<div id='search_result_header'><h2>Search Result</h2></div")})
+                                      search_city_bool = city_sf_final$search_matching %in% search_results
+                                      city_sf_final$search = search_city_bool
+                                      
+                                      city_row <-subset(city_sf_final, 
+                                                        city_sf_final$search == 1)
+                                      
+                                      copy_pos_search = data.frame(city_row)
+                                      table_display = select(copy_pos_search,
+                                                             c('NAMELSAD',
+                                                               'filepath_2'))
+                                      
+                                      table_display$filepath_2 = paste0("<a href='",
+                                                                        table_display$filepath_2,
+                                                                        "' target='_blank'>",
+                                                                        table_display$filepath_2,
+                                                                        "</a>")
+                                      
+                                      names(table_display) = c('Name', 
+                                                               'Link')
+                                      
+                                      #copy_city_sf_final = sapply(copy_city_sf_final,unlist)
+                                      
+                                      output$search_result_table <- DT::renderDataTable(table_display, 
+                                                                                        escape = FALSE)
+                                      
+                                      toClear = subset(city_sf_final, 
+                                                       city_sf_final$search == 0)
+                                      
+                                      #things for highlighting selected cities 
+                                      leafletProxy("mymap", session) %>% 
+                                        addPolygons(data = city_row, 
+                                                    layerId = ~NAME,
+                                                    label = ~NAME,
+                                                    color = "#444444",
+                                                    opacity = 1.0,
+                                                    weight = 0.5,
+                                                    smoothFactor = 1.5,
+                                                    fillColor = "#00bfc4",
+                                                    fillOpacity = 0.9,
+                                                    highlightOptions = highlightOptions(color = "white",
+                                                                                        weight = 1,
+                                                                                        bringToFront = TRUE), 
+                                                    labelOptions = labelOptions(style = list("font-weight" = "normal",
+                                                                                             padding = "3px 8px"),
+                                                                                textsize = "10px",
+                                                                                direction = "auto"))
+                                      if(nrow(toClear != 0)){leafletProxy("mymap", session) %>%
+                                                               addPolygons(data = toClear,
+                                                                           layerId = ~NAME,
+                                                                           label = ~NAME,
+                                                                           color = "#444444",
+                                                                           opacity = 1.0,
+                                                                           weight = 0.5,
+                                                                           smoothFactor = 1.5,
+                                                                           fillColor = "#f8766d",
+                                                                           fillOpacity = 0.9,
+                                                                           highlightOptions = highlightOptions(color = "white",
+                                                                                                               weight = 1,
+                                                                                                               bringToFront = TRUE),
+                                                                           labelOptions = labelOptions(style = list("font-weight" = "normal",
+                                                                                                                    padding = "3px 8px"),
+                                                                                                       textsize = "10px",
+                                                                                                       direction = "auto"))}
+                                      else{print("Term appears in all plans.")}
+                                      }
+                                      print("search for things complete")}})
 
   observeEvent(input$clear_map, {print("clear map button clicked"); 
                                 
